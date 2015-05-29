07025ec0394a859819584ff82f600e43ed77de5c
Michael M <mike@madgett.io>
2015-05-29 01:22:04 -0500

    gsync framework

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
 # gsync
-Asynchronous control flow with generators
+gsync is an asynchronous control flow framework for use with es6 generators. The framework models existing Javascript callback methodology, resulting in easier adoption of es6 generators into existing projects.
+
+## Control Flow Functions
+* `gsync.series([], function(err, result))`
+* `gsync.parallel([], function(err, result))`
+
+## Examples
+
+**gsync.series**
+Execute generators and functions in the order specified, including nested generators.
+```
+function doSomeWork(param, callback) {
+  setTimeout(function() { callback(null, param.url); }, Math.random() * 500);
+}
+
+gsync.series([
+  // generator
+  function*(next) {
+    var json = yield doSomeWork({ url: 'example.com/series/?1a' }, next);
+    console.log(json);
+    var moreJson = yield doSomeWork({ url: 'example.com/series/?1b' }, next);
+    console.log(moreJson);
+  },
+  // generator
+  function*(next) {
+    var json = yield doSomeWork({ url: 'example.com/series/?2a' }, next);
+    console.log(json);
+    var moreJson = yield doSomeWork({ url: 'example.com/series/?2b' }, next);
+    console.log(moreJson);
+  },
+  // Mix non-generator callbacks
+  function(next) {
+    doSomeWork({ url: 'example.com/series/?3' }, function(err, result) {
+      console.log(result);
+      next(err);
+    });
+  }
+], function (err) {
+  console.log('gsync.series done!');
+  done();
+});
+
+```
+
+**gsync.parallel**
+Execute all generators and functions as fast as possible in parallel, including nested generators. Note that generators are inherently serial (iterators) and will internally execute their respective yields as such. However, generators specified in the top level array will continue executing in parallel.
+```
+function doSomeWork(param, callback) {
+  setTimeout(function() { callback(null, param.url); }, Math.random() * 500);
+}
+
+gsync.parallel([
+  // generator
+  function*(next) {
+    var json = yield doSomeWork({ url: 'example.com/parallel/?1a' }, next);
+    console.log(json);
+    var moreJson = yield doSomeWork({ url: 'example.com/parallel/?1b' }, next);
+    console.log(moreJson);
+  },
+  // generator
+  function*(next) {
+    var json = yield doSomeWork({ url: 'example.com/parallel/?2a' }, next);
+    console.log(json);
+    var moreJson = yield doSomeWork({ url: 'example.com/parallel/?2b' }, next);
+    console.log(moreJson);
+  },
+  // Mix non-generator callbacks
+  function(next) {
+    doSomeWork({ url: 'example.com/parallel/?3' }, function(err, result) {
+      console.log(result);
+      next(err);
+    });
+  },
+  // generator
+  function*(next) {
+    var json = yield doSomeWork({ url: 'example.com/parallel/?4' }, next);
+    console.log(json);
+  }
+], function (err) {
+  console.log('gsync.parallel done!');
+  done();
+});
+
+```
---
+++ test/index.js
@@ -0,0 +1,420 @@
+var should = require('should'),
+    debug = require('debug'),
+    gsync = require('../lib/gsync');
+
+describe('gsync', function() {
+  
+  function asyncFunc(param, callback) {
+    setTimeout(function() { callback(param.error, param.value); }, param.delay);
+  }
+  function syncFunc(param, callback) {
+    callback(param.error, param.value);
+  }
+
+  describe('series', function() {
+
+    it('gsync.series should process synchronous generators', function(done) {
+      var values = ['1a', '1b', '2', '3a', '3b'];
+      var validate = [];
+      gsync.series([
+        function*(next) {
+          var result = yield syncFunc({ error: null, value: values[0] }, next);
+          validate.push(result);
+
+          result = yield syncFunc({ error: null, value: values[1] }, next);
+          validate.push(result);
+        },
+        function*(next) {
+          var result = yield syncFunc({ error: null, value: values[2] }, next);
+          validate.push(result);
+        },
+        function*(next) {
+          var result = yield syncFunc({ error: null, value: values[3] }, next);
+          validate.push(result);
+
+          result = yield syncFunc({ error: null, value: values[4] }, next);
+          validate.push(result);
+        }
+      ], function (err) {
+        should(err).not.be.ok;
+        validate.should.be.eql(values);
+        done();
+      });
+    });
+
+    it('gsync.series should process asynchronous generators', function(done) {
+      var values = ['11a', '11b', '12', '13a', '13b'];
+      var validate = [];
+      gsync.series([
+        function*(next) {
+          var result = yield asyncFunc({ error: null, value: values[0], delay: Math.random() * 300 }, next);
+          validate.push(result);
+
+          result = yield asyncFunc({ error: null, value: values[1], delay: Math.random() * 300 }, next);
+          validate.push(result);
+        },
+        function*(next) {
+          var result = yield asyncFunc({ error: null, value: values[2], delay: Math.random() * 300 }, next);
+          validate.push(result);
+        },
+        function*(next) {
+          var result = yield asyncFunc({ error: null, value: values[3], delay: Math.random() * 300 }, next);
+          validate.push(result);
+
+          result = yield asyncFunc({ error: null, value: values[4], delay: Math.random() * 300 }, next);
+          validate.push(result);
+        }
+      ], function (err) {
+        should(err).not.be.ok;
+        validate.should.be.eql(values);
+        done();
+      });
+    });
+
+    it('gsync.series should be able to mix asynchronous generators and functions', function(done) {
+      var values = ['21a', '21b', '22', '23a', '23b'];
+      var validate = [];
+      gsync.series([
+        function*(next) {
+          var result = yield asyncFunc({ error: null, value: values[0], delay: Math.random() * 300 }, next);
+          validate.push(result);
+
+          result = yield asyncFunc({ error: null, value: values[1], delay: Math.random() * 300 }, next);
+          validate.push(result);
+        },
+        function(next) {
+          asyncFunc({ error: null, value: values[2], delay: Math.random() * 300 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        },
+        function*(next) {
+          var result = yield asyncFunc({ error: null, value: values[3], delay: Math.random() * 300 }, next);
+          validate.push(result);
+
+          result = yield asyncFunc({ error: null, value: values[4], delay: Math.random() * 300 }, next);
+          validate.push(result);
+        }
+      ], function (err) {
+        should(err).not.be.ok;
+        validate.should.be.eql(values);
+        done();
+      });
+    });
+
+    it('gsync.series should be able to mix synchronous generators and functions', function(done) {
+      var values = ['31a', '31b', '32', '33a', '33b', '34'];
+      var validate = [];
+      gsync.series([
+        function*(next) {
+          var result = yield syncFunc({ error: null, value: values[0] }, next);
+          validate.push(result);
+
+          result = yield syncFunc({ error: null, value: values[1], delay: Math.random() * 300 }, next);
+          validate.push(result);
+        },
+        function(next) {
+          asyncFunc({ error: null, value: values[2], delay: Math.random() * 300 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        },
+        function*(next) {
+          var result = yield syncFunc({ error: null, value: values[3] }, next);
+          validate.push(result);
+
+          result = yield syncFunc({ error: null, value: values[4] }, next);
+          validate.push(result);
+        },
+        function(next) {
+          asyncFunc({ error: null, value: values[5], delay: Math.random() * 300 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        }
+      ], function (err) {
+        should(err).not.be.ok;
+        validate.should.be.eql(values);
+        done();
+      });
+    });
+
+    it('gsync.series should be able to process all functions', function(done) {
+      var values = ['41', '42', '43', '44'];
+      var validate = [];
+      gsync.series([
+        function(next) {
+          asyncFunc({ error: null, value: values[0], delay: Math.random() * 300 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        },
+        function(next) {
+          asyncFunc({ error: null, value: values[1], delay: Math.random() * 300 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        },
+        function(next) {
+          asyncFunc({ error: null, value: values[2], delay: Math.random() * 300 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        },
+        function(next) {
+          asyncFunc({ error: null, value: values[3], delay: Math.random() * 300 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        }
+      ], function (err) {
+        should(err).not.be.ok;
+        validate.should.be.eql(values);
+        done();
+      });
+    });
+
+  });
+
+  describe('parallel', function() {
+
+    it('gsync.parallel should process synchronous generators', function(done) {
+      var values = ['1a', '1b', '2', '3a', '3b'];
+      var validate = [];
+      gsync.parallel([
+        function*(next) {
+          var result = yield syncFunc({ error: null, value: values[0] }, next);
+          validate.push(result);
+
+          result = yield syncFunc({ error: null, value: values[1] }, next);
+          validate.push(result);
+        },
+        function*(next) {
+          var result = yield syncFunc({ error: null, value: values[2] }, next);
+          validate.push(result);
+        },
+        function*(next) {
+          var result = yield syncFunc({ error: null, value: values[3] }, next);
+          validate.push(result);
+
+          result = yield syncFunc({ error: null, value: values[4] }, next);
+          validate.push(result);
+        }
+      ], function (err) {
+        should(err).not.be.ok;
+        validate.should.be.an.instanceOf(Array).and.have.lengthOf(values.length);
+        validate.should.not.be.eql(values);
+        done();
+      });
+    });
+
+    it('gsync.parallel should process asynchronous generators', function(done) {
+      var values = ['11a', '11b', '12', '13a', '13b'];
+      var validate = [];
+      gsync.parallel([
+        function*(next) {
+          var result = yield asyncFunc({ error: null, value: values[0], delay: 1000 }, next);
+          validate.push(result);
+
+          result = yield asyncFunc({ error: null, value: values[1], delay: Math.random() * 300 }, next);
+          validate.push(result);
+        },
+        function*(next) {
+          var result = yield asyncFunc({ error: null, value: values[2], delay: Math.random() * 800 }, next);
+          validate.push(result);
+        },
+        function*(next) {
+          var result = yield asyncFunc({ error: null, value: values[3], delay: Math.random() * 300 }, next);
+          validate.push(result);
+
+          result = yield asyncFunc({ error: null, value: values[4], delay: 1000 }, next);
+          validate.push(result);
+        }
+      ], function (err) {
+        should(err).not.be.ok;
+        validate.should.be.an.instanceOf(Array).and.have.lengthOf(values.length);
+        validate.should.not.be.eql(values);
+        done();
+      });
+    });
+
+    it('gsync.parallel should be able to mix asynchronous generators and functions', function(done) {
+      var values = ['21a', '21b', '22', '23a', '23b'];
+      var validate = [];
+      gsync.parallel([
+        function*(next) {
+          var result = yield asyncFunc({ error: null, value: values[0], delay: 1000 }, next);
+          validate.push(result);
+
+          result = yield asyncFunc({ error: null, value: values[1], delay: Math.random() * 300 }, next);
+          validate.push(result);
+        },
+        function(next) {
+          asyncFunc({ error: null, value: values[2], delay: Math.random() * 300 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        },
+        function*(next) {
+          var result = yield asyncFunc({ error: null, value: values[3], delay: Math.random() * 300 }, next);
+          validate.push(result);
+
+          result = yield asyncFunc({ error: null, value: values[4], delay: 1000 }, next);
+          validate.push(result);
+        }
+      ], function (err) {
+        should(err).not.be.ok;
+        validate.should.be.an.instanceOf(Array).and.have.lengthOf(values.length);
+        validate.should.not.be.eql(values);
+        done();
+      });
+    });
+
+    it('gsync.parallel should be able to mix synchronous generators and functions', function(done) {
+      var values = ['31a', '31b', '32', '33a', '33b', '34'];
+      var validate = [];
+      gsync.parallel([
+        function*(next) {
+          var result = yield syncFunc({ error: null, value: values[0] }, next);
+          validate.push(result);
+
+          result = yield syncFunc({ error: null, value: values[1], delay: Math.random() * 300 }, next);
+          validate.push(result);
+        },
+        function(next) {
+          asyncFunc({ error: null, value: values[2], delay: 100 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        },
+        function*(next) {
+          var result = yield syncFunc({ error: null, value: values[3] }, next);
+          validate.push(result);
+
+          result = yield syncFunc({ error: null, value: values[4] }, next);
+          validate.push(result);
+        },
+        function(next) {
+          asyncFunc({ error: null, value: values[5], delay: 100 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        }
+      ], function (err) {
+        should(err).not.be.ok;
+        validate.should.be.an.instanceOf(Array).and.have.lengthOf(values.length);
+        validate.should.not.be.eql(values);
+        done();
+      });
+    });
+
+    it('gsync.parallel should be able to process all functions', function(done) {
+      var values = ['41', '42', '43', '44'];
+      var validate = [];
+      gsync.parallel([
+        function(next) {
+          asyncFunc({ error: null, value: values[0], delay: 1000 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        },
+        function(next) {
+          asyncFunc({ error: null, value: values[1], delay: 1000 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        },
+        function(next) {
+          asyncFunc({ error: null, value: values[2], delay: 100 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        },
+        function(next) {
+          asyncFunc({ error: null, value: values[3], delay: 100 }, function(err, val) {
+            validate.push(val);
+            next(err);
+          });
+        }
+      ], function (err) {
+        should(err).not.be.ok;
+        validate.should.be.an.instanceOf(Array).and.have.lengthOf(values.length);
+        validate.should.not.be.eql(values);
+        done();
+      });
+    });
+
+    function doSomeWork(param, callback) {
+      setTimeout(function() { callback(null, param.url); }, Math.random() * 500);
+    }
+
+    it('sample series', function(done) {
+
+      gsync.series([
+        // generator
+        function*(next) {
+          var json = yield doSomeWork({ url: 'example.com/series/?1a' }, next);
+          console.log(json);
+          var moreJson = yield doSomeWork({ url: 'example.com/series/?1b' }, next);
+          console.log(moreJson);
+        },
+        // generator
+        function*(next) {
+          var json = yield doSomeWork({ url: 'example.com/series/?2a' }, next);
+          console.log(json);
+          var moreJson = yield doSomeWork({ url: 'example.com/series/?2b' }, next);
+          console.log(moreJson);
+        },
+        // Mix non-generator callbacks
+        function(next) {
+          doSomeWork({ url: 'example.com/series/?3' }, function(err, result) {
+            console.log(result);
+            next(err);
+          });
+        }
+      ], function (err) {
+        console.log('gsync.series Done!');
+        done();
+      });
+
+    });
+
+    it('sample parallel', function(done) {
+
+      gsync.parallel([
+        // generator
+        function*(next) {
+          var json = yield doSomeWork({ url: 'example.com/parallel/?1a' }, next);
+          console.log(json);
+          var moreJson = yield doSomeWork({ url: 'example.com/parallel/?1b' }, next);
+          console.log(moreJson);
+        },
+        // generator
+        function*(next) {
+          var json = yield doSomeWork({ url: 'example.com/parallel/?2a' }, next);
+          console.log(json);
+          var moreJson = yield doSomeWork({ url: 'example.com/parallel/?2b' }, next);
+          console.log(moreJson);
+        },
+        // Mix non-generator callbacks
+        function(next) {
+          doSomeWork({ url: 'example.com/parallel/?3' }, function(err, result) {
+            console.log(result);
+            next(err);
+          });
+        },
+        // generator
+        function*(next) {
+          var json = yield doSomeWork({ url: 'example.com/parallel/?4' }, next);
+          console.log(json);
+        }
+      ], function (err) {
+        console.log('gsync.parallel Done!');
+        done();
+      });
+
+    });
+
+  });
+
+});
